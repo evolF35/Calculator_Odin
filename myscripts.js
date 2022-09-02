1bdd836b1ce6438a3f199f9198e60b6ce38b33ba
evolF35 <maccyair@gmail.com>
2022-09-01 23:51:37 -0700

    Restructed and having working equals button

--- myscripts.js
+++ myscripts.js
@@ -18,73 +18,129 @@ function divide(x,y){
 function operate(x,y,ops){
     if(ops == '+'){
         return(add(x,y));}
-    if(ops == '-'){
+    if(ops == '−'){
             return(subtract(x,y));}
-    if(ops == '*'){
+    if(ops == '×'){
             return(multiply(x,y));}
-    if(ops == '/'){
+    if(ops == '÷'){
             return(divide(x,y));}
 }
 
-function checkPeriod(string){
-    for(let i = 0; i < string.length; i++){
-        if(string[i] == '.'){
-            return(false);
-        }
-    }
-    return(true);
-}
 
-function checkOps(string){
-    for(let i = 0; i < string.length; i++){
-        if(string[i] == `+` || 
-        string[i] == '−' || 
-        string[i] == '×' || 
-        string[i] == `÷` ){
-            return(false);
-        }
-    }
-    return(true);
-}
+let equationString = "";
+let equationEnd = 0;
+
+let currentOperator = "";
+let operand1 = 0;
+let operand2 = 0;
 
 
-let topOutput = ``;
-let result = ``;
+let hasPeriod = false;
+let operatorOn = false;
 
-let op1 = 0;
-let op2 = 0;
-
-document.getElementById('topoutput').innerText = ` `;
-document.getElementById('bottomoutput').innerText = " ";
+let op1String = "";
+let op2String = "";
 
 
 let numberButtons = document.querySelectorAll('.number');
 
 for (i of numberButtons) {
-  i.addEventListener('click', function() {
-    topOutput += `${this.innerText}`;
-    document.getElementById('topoutput').innerText = (topOutput);
+    i.addEventListener('click', function() {
+        if(operatorOn == false){
+            op1String += `${this.innerText}`;
+        }
+        else if(operatorOn == true){
+            op2String += `${this.innerText}`;
+        }
+
+        equationString += `${this.innerText}`;
+        document.getElementById('topoutput').innerText = equationString;
 });
 }
+
+let zero = document.querySelector('#zero');
+
+zero.addEventListener('click', () => {
+    if(equationString.length != 0){
+        if(operatorOn == false){
+            op1String += `0`;
+        }
+        else if(operatorOn == true){
+            op2String += `0`;
+        }
+        equationString += '0';
+        document.getElementById('topoutput').innerText = equationString;
+    }
+});
+
+let operators = document.querySelectorAll('.operator');
+
+for (j of operators) {
+    j.addEventListener('click', function() {
+        if(equationString.length != 0 && operatorOn != true){
+
+            equationString += `${this.innerText}`;
+            document.getElementById('topoutput').innerText = equationString; 
+
+            currentOperator = `${this.innerText}`;
+
+            operatorOn = true;
+    }
+  });
+}
+
+let Equa = document.querySelector('#Equal');
+
+
+Equa.addEventListener('click', () => {
+    
+    let op1 = parseFloat(op1String);
+    let op2 = parseFloat(op2String);
+    let opR = currentOperator;
+
+    let end = operate(op1,op2,opR);
+    document.getElementById('bottomoutput').innerText = end; 
+})
+
+
+
+
+
+/*
+let topOutput = '';
+let result = '';
+
+
+
+
+
+let thereOps = false;
+let currentOps = '';
+
+
+
+
+document.getElementById('topoutput').innerText = ` `;
+document.getElementById('bottomoutput').innerText = " ";
+
 
 let operators = document.querySelectorAll('.operator');
 
 for (j of operators) {
     j.addEventListener('click', function() {
 
-    if(checkOps(topOutput) == true){
-      op1 = parseFloat(topOutput);
+    if(thereOps == false){
       topOutput += `${this.innerText}`;
-      console.log(this.innerText);
-      document.getElementById('topoutput').innerText = (topOutput); }
-    else{
-        op2 = 
+      document.getElementById('topoutput').innerText = topOutput; 
+      currentOps = `${this.innerText}`;
+      console.log(currentOps);
+      topOutput = '';
     }
-    
+    else{
 
+    }
   });
-  }
-
+}
 
 
 let decimal = document.querySelector('#Decimal');
@@ -114,15 +170,11 @@ Del.addEventListener('click',() => {
 });
 
 
-let Equa = document.querySelector('#Equal');
 
 
-Equa.addEventListener('click', () => {
-
-})
 
 
-checkOps('potatoz+')
+*/
 
 
 
